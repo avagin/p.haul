644976739663028544924912e669bdd998a25c3b
Andrey Vagin <avagin@openvz.org>
2014-03-19 20:20:22 +0400

    netapi: add the net_dev class
    It's more readable than a tuple
    
    * I have not tested this pathc for OpenVZ

--- p_haul_ovz.py
+++ p_haul_ovz.py
@@ -7,6 +7,7 @@ import shutil
 import p_haul_cgroup
 import p_haul_netifapi as netif
 import p_haul_fsapi as fsapi
+import p_haul_netapi as netapi
 import fs_haul_shared
 import fs_haul_subtree
 
@@ -58,7 +59,11 @@ class p_haul_type:
 
 				if v_in and v_out:
 					print "\tCollect %s -> %s (%s) veth" % (v_in, v_out, v_bridge)
-					self._veths.append((v_in, v_out, v_bridge))
+					veth = netapi.net_dev()
+					veth.name = v_in
+					veth.pair = v_out
+					veth.link = v_bridge
+					self._veths.append(veth)
 
 		ifd.close()
 
